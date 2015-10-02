f6d2d915ef8c6811373c01a602773745ac0f33ee
silentcanon <silentcanon@gmail.com>
2015-10-02 01:22:17 -0400

    added functions

--- photo/forms.py
+++ photo/forms.py
@@ -1,11 +1,17 @@
 __author__ = 'Canon'
 
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, FileField, IntegerField
 from wtforms.validators import DataRequired
 
 class photoForm(Form):
-    title = StringField('Title', validators=[DataRequired])
-    description = StringField('Description', validators=[DataRequired])
-    image = FileField('Image', validators=[DataRequired])
+    title = StringField('title')
+    x1 = IntegerField('x1', validators=[DataRequired()])
+    x2 = IntegerField('x2', validators=[DataRequired()] )
+    y1 = IntegerField('y1', validators=[DataRequired()])
+    y2 = IntegerField('y2', validators=[DataRequired()])
+    w = IntegerField('w', validators=[DataRequired()])
+    h = IntegerField('h', validators=[DataRequired()])
+    description = StringField('description')
+    image_upload = FileField('image_upload', validators=[DataRequired()])
 
--- photo/views.py
+++ photo/views.py
@@ -2,7 +2,6 @@ __author__ = 'Canon'
 from . import photo
 from flask import redirect, render_template, url_for, request
 from flask.ext.login import login_required, current_user
-import datetime
 from app.decorators import admin_required, admin_required_json
 from .forms import photoForm
 
@@ -15,4 +14,11 @@ import json
 def photo_uploder():
     photo_form = photoForm()
     if not photo_form.validate_on_submit():
-        return render_template('photo_uploader.html')
+        print photo_form.errors
+        return render_template('photo_uploader.html', photoForm=photo_form)
+    title = photo_form.title.data
+    description = photo_form.description.data
+    print title
+    print description
+    return redirect(url_for('gallery.index'))
+
